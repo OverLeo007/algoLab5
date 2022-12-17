36df044a859bf11c876957d3da7f09c7caa78c5b
overleo007 <Sokolov_lev2003@mail.ru>
2022-12-17 14:18:21 +0700

    upd

---
+++ internal_sort.py
@@ -0,0 +1,75 @@
+from sys import getrecursionlimit
+
+from typing import Optional, Callable
+
+
+def merge_sort(array: list, reverse: bool = False,
+               key: Optional[Callable] = None,
+               cmp: Optional[Callable] = None) -> list:
+    """
+    Реализация сортировки слиянием
+
+    @param array: сортируемый массив
+    @param reverse: сортируем напрямую или в обратную сторону
+    @param key: ключ сортировки в виде функции
+    @param cmp: компаратор для значений
+    @return: отсортированный массив
+    """
+    key = key if key is not None else lambda x: x
+    cmp = cmp if cmp is not None else lambda x, y: x < y
+
+    def insertion_sort(arr):
+        for i in range(1, len(arr)):
+            temp = arr[i]
+            j = i - 1
+            while j >= 0 and \
+                    (cmp(key(temp),  key(arr[j])) if reverse else cmp(key(arr[j]),  key(temp))):
+                arr[j + 1] = arr[j]
+                j -= 1
+            arr[j + 1] = temp
+
+    def merge_sort(arr: list, depth: int = 1) -> list:
+
+        if (n_len := len(arr)) > 1:
+            mid = n_len // 2
+            left = arr[:mid]
+            right = arr[mid:]
+            if depth + 1 > getrecursionlimit():
+                insertion_sort(left)
+                insertion_sort(right)
+            else:
+                merge_sort(left, depth + 1)
+                merge_sort(right, depth + 1)
+
+            i = j = k = 0
+
+            while i < len(left) and j < len(right):
+                if cmp(key(left[i]), key(right[j])) \
+                        if not reverse else \
+                        cmp(key(right[j]), key(left[i])):
+                    arr[k] = left[i]
+                    i += 1
+                else:
+                    arr[k] = right[j]
+                    j += 1
+                k += 1
+
+            while j < len(right):
+                arr[k] = right[j]
+                j += 1
+                k += 1
+
+            while i < len(left):
+                arr[k] = left[i]
+                i += 1
+                k += 1
+        return arr
+
+    return merge_sort(array)
+
+
+if __name__ == '__main__':
+    from random import randint
+
+    lst = [randint(1, 1000) for i in range(1000)]
+    print(merge_sort(lst))
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import io
+from io import UnsupportedOperation
 import os
 
-FILE_DIR = r"C:\Users\leva\PycharmProjects\algoLab5\files"
+from internal_sort import merge_sort
+
+FILE_DIR = r"C:\Users\sokol\PycharmProjects\algoLab5\files"
 
 
 class IO:
@@ -12,7 +14,7 @@ class IO:
 
     def read(self):
         if self.mode != "r":
-            raise io.UnsupportedOperation("not readable")
+            raise UnsupportedOperation("not readable")
         res = []
         while True:
             char = self.file.read(1)
@@ -23,17 +25,39 @@ class IO:
             res.append(char)
         return False
 
+    def read_buffer(self, buffer_size):
+        res = []
+        for i in range(buffer_size):
+            if num := self.read():
+                res.append(num)
+            else:
+                break
+        return res
+
+    def get_buffer(self, buffer_size):
+        res = []
+        for i in range(buffer_size):
+            num = self.read()
+            if num is False:
+                return res
+            res.append(num)
+        return res
+
     def change_mode(self, new_mode):
         self.mode = new_mode
         self.file.close()
         self.file = open(os.path.join(FILE_DIR, self.filename), self.mode)
 
     def write(self, num):
-        if not isinstance(num, int):
-            raise ValueError
+        # if not isinstance(num, int):
+        #     raise ValueError
         if self.mode != "w":
-            raise io.UnsupportedOperation("not writable")
+            raise UnsupportedOperation("not writable")
         self.file.write(f"{num}\n")
+
+    def write_buffer(self, buffer):
+        for el in buffer:
+            self.write(el)
 
     def __del__(self):
         print("file", self.filename, "closed")
@@ -43,116 +67,102 @@ class IO:
 def generate_input(file_name="input.txt"):
     from random import shuffle
     with open(os.path.join(FILE_DIR, file_name), "w") as file:
-        res = [i for i in range(1, 21)]
+        res = [i for i in range(1, 100)]
         shuffle(res)
         for i in res:
             file.write(f"{i}\n")
 
 
-def sort(filename="input.txt"):
+def sort(buffer_size=1000, filename="input.txt"):
     inp = IO(filename, "r")
-    f1 = IO("line1.txt", "w")
-    f2 = IO("line2.txt", "w")
+    tape1 = IO("line1.txt", "w")
+    tape2 = IO("line2.txt", "w")
 
-    f3 = IO("line3.txt", "w")
-    # f4 = IO("line4.txt", "w")
+    tape3 = IO("line3.txt", "w")
+    tape4 = IO("line4.txt", "w")
 
     def split():
-        inp.change_mode("r")
-        f1.change_mode("w")
-        f2.change_mode("w")
 
-        is_low_line = True
-        prev_num = inp.read()
-        is_ended = False
+        is_tape1 = True
+
         while True:
-            if is_ended:
+            buf = inp.read_buffer(buffer_size)
+            merge_sort(buf)
+            if is_tape1:
+                tape1.write_buffer(buf)
+            else:
+                tape2.write_buffer(buf)
+
+            is_tape1 = not is_tape1
+
+            if len(buf) != buffer_size:
                 break
-            if is_low_line:
-                cur_out_line = f1
-            else:
-                cur_out_line = f2
 
-            # print(prev_num)
-            cur_out_line.write(prev_num)
+    def merge(from_tape_num):
+        if from_tape_num == 1:
+            tape1.change_mode("r")
+            tape2.change_mode("r")
+            tape3.change_mode("w")
+            tape4.change_mode("w")
+            read1, read2 = tape1, tape2
+            write1, write2 = tape3, tape4
+        else:
+            tape1.change_mode("w")
+            tape2.change_mode("w")
+            tape3.change_mode("r")
+            tape4.change_mode("r")
+            read1, read2 = tape3, tape4
+            write1, write2 = tape1, tape2
 
-            while True:
+        # print(write1.filename, write2.filename, read1.filename, read2.filename)
 
-                if not (next_num := inp.read()):
-                    is_ended = True
-                    break
+        is_write1 = True
+        last_seq = False
 
-                if next_num < prev_num:
-                    is_low_line = not is_low_line
-                    prev_num = next_num
-                    break
+        while True:
+
+            if last_seq:
+                break
+
+            seq1 = read1.read_buffer(buffer_size)
+            seq2 = read2.read_buffer(buffer_size)
+            print(seq1, seq2, sep="\n")
+            i = 0
+            j = 0
+            while i < len(seq1) and j < len(seq2):
+                if seq1[i] < seq2[j]:
+                    if is_write1:
+                        write1.write(seq1[i])
+                    else:
+                        write2.write(seq1[i])
+                    i += 1
                 else:
-                    prev_num = next_num
-                    cur_out_line.write(next_num)
+                    if is_write1:
+                        write1.write(seq2[j])
+                    else:
+                        write2.write(seq2[j])
+                    j += 1
 
-    def merge(out=inp):
+            while i < len(seq1):
+                if is_write1:
+                    write1.write(seq1[i])
+                else:
+                    write2.write(seq1[i])
+                i += 1
 
-        f1.change_mode("r")
-        f2.change_mode("r")
+            while j < len(seq2):
+                if is_write1:
+                    write1.write(seq2[j])
+                else:
+                    write2.write(seq2[j])
+                j += 1
 
-        f1_next = f1.read()
+            is_write1 = not is_write1
+            if len(seq1) != len(seq2):
+                last_seq = True
 
-        if not (f2_next := f2.read()):
-            return True
-
-        out.change_mode("w")
-
-        f1_end = False
-        f2_end = False
-        f1_count = 0
-        f2_count = 0
-
-        while not (f1_end or f2_end):
-            if f1_next < f2_next:
-                out.write(f1_next)
-                f1_count += 1
-
-                if not (f1_next := f1.read()):
-                    f1_end = True
-            else:
-                f2_count += 1
-                out.write(f2_next)
-
-                if not (f2_next := f2.read()):
-                    f2_end = True
-
-        if not f1_end:
-            while True:
-                if not (f1_next := f1.read()):
-                    print()
-                    break
-
-                out.write(f1.read())
-                f1_count += 1
-                print("f1_add", f1_next)
-
-        if not f2_end:
-            while True:
-                if not (f2_next := f2.read()):
-                    print()
-                    break
-
-                out.write(f2.read())
-                f2_count += 1
-                print("f2_add", f2_next)
-        print(f1_count, f2_count)
-
-    # while True:
-    #     split()
-    #     if merge(f3):
-    #         break
     split()
-    # merge()
-    # split()
-    # merge()
-    # split()
-    # merge()
-    # split()
+    merge(1)
 
 
 def main():
@@ -162,6 +172,6 @@ def main():
 
 
 if __name__ == '__main__':
-    generate_input()
-    sort()
+    # generate_input()
+    sort(buffer_size=10)
     # main()
